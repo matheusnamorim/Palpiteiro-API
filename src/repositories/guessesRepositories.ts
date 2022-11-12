dbfd94dfa68c375145a0f1322b3ed52208357748
Matheus <matheusnavesamorim@gmail.com>
2022-11-12 15:39:07 -0300

    Fix: database

--- src/repositories/guessesRepositories.ts
+++ src/repositories/guessesRepositories.ts
@@ -1,4 +1,4 @@
-import { connection } from "../../database/db.js";
+import { connection } from "../database/db.js";
 import { QueryResult } from 'pg';
 import { Games } from "../protocols/Guesses.js";
 
